acf98a59935089e5b4031d0b8b913bac8dc749ba
augustustung <huytung139@gmail.com>
2022-12-06 19:35:29 +0700

    building SC rent NFT

---
+++ contracts/test/NFT.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT is ERC721 {
+    address public owner;
+    uint256 public tokenCounter = 1;
+
+    constructor(string memory name_, string memory symbol_)
+        ERC721(name_, symbol_)
+    {
+        owner = msg.sender;
+    }
+
+    function mint(uint256 _amount) external {
+        for (uint256 i = 0; i < _amount; i++) {
+            _mint(msg.sender, tokenCounter);
+            tokenCounter++;
+        }
+    }
+}
